35446c8987fa139a369d3e4b01da788fd61fcac7
Mabs Ademola <mabsademola@gmail.com>
2025-02-05 09:23:17 +0100

    #

--- hafar_backend/settings copy.py
+++ hafar_backend/settings copy.py
@@ -27,6 +27,9 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'channels',
+    'storages',
+
+    # HAFAR APPS
     'chat',
     'users',
     'match',
@@ -37,7 +40,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    
+
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -47,6 +50,40 @@ MIDDLEWARE = [
     # ... other middleware ...
     # 'users.middleware.UpdateLastSeenMiddleware',
 ]
+
+# if not DEBUG:
+if DEBUG:
+    # Production settings
+    # Use django-storages and boto3 for Amazon S3
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+        
+    AWS_ACCESS_KEY_ID = config('DO_SPACES_KEY')
+    AWS_SECRET_ACCESS_KEY = config('DO_SPACES_SECRET')
+    AWS_STORAGE_BUCKET_NAME = config('DO_SPACES_BUCKET')
+    AWS_S3_ENDPOINT_URL = config('DO_SPACES_ENDPOINT')
+    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+    AWS_S3_CUSTOM_DOMAIN = config('DO_SPACES_CUSTOM_DOMAIN')
+    STATIC_URL = f'{AWS_S3_CUSTOM_DOMAIN}/static/'
+    MEDIA_URL = f'{AWS_S3_CUSTOM_DOMAIN}/media/'
+else:
+    STATIC_URL = '/static/'
+    MEDIA_URL = '/media/'
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.1/howto/static-files/
+
+# STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Media files (Uploaded files)
+# MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 ROOT_URLCONF = 'hafar_backend.urls'
 
@@ -113,12 +150,15 @@ CHANNEL_LAYERS = {
 
 AUTH_USER_MODEL = 'users.user'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 # Media configuration
 FILE_UPLOAD_MAX_MEMORY_SIZE = 104857600  # 100MB
 DATA_UPLOAD_MAX_MEMORY_SIZE = 104857600  # 100MB
+
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
 
 # Database
@@ -185,11 +225,6 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.1/howto/static-files/
-
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
