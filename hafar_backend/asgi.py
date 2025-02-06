b6ecd8dd51c781fb0dfed0f76db46de7352b59ca
Mabs Ademola <mabsademola@gmail.com>
2025-02-06 09:50:57 +0100

    #

--- hafar_backend/asgi.py
+++ hafar_backend/asgi.py
@@ -1,18 +1,13 @@
-
 import os
-
-from django.core.asgi import get_asgi_application
-import os
-from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-# from channels.auth import AuthMiddlewareStack
-from chat.routing import websocket_urlpatterns
-from .middleware import JwtAuthMiddleware
+import django
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hafar_backend.settings')
+django.setup()
 
-# application = get_asgi_application()
-
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from .middleware import JwtAuthMiddleware
+from chat.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
@@ -20,3 +15,26 @@ application = ProtocolTypeRouter({
         URLRouter(websocket_urlpatterns)
     ),
 })
+
+
+# import os
+
+# from django.core.asgi import get_asgi_application
+# import os
+# from django.core.asgi import get_asgi_application
+# from channels.routing import ProtocolTypeRouter, URLRouter
+# # from channels.auth import AuthMiddlewareStack
+# from chat.routing import websocket_urlpatterns
+# from .middleware import JwtAuthMiddleware
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hafar_backend.settings')
+
+# # application = get_asgi_application()
+
+
+# application = ProtocolTypeRouter({
+#     "http": get_asgi_application(),
+#     "websocket": JwtAuthMiddleware(
+#         URLRouter(websocket_urlpatterns)
+#     ),
+# })
