8e3d3a3ca6bdb61429125ecd174c50830b33fcba
Mabs Ademola <mabsademola@gmail.com>
2025-02-06 13:13:35 +0100

    #

--- hafar_backend/urls.py
+++ hafar_backend/urls.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework import views, response
+
+class APIRootView(views.APIView):
+    """
+    A simple view to respond to GET requests to the root of the API.
+    """
+    def get(self, request):
+        return views.Response({"message": "Welcome to the API. Use ____ for API access."})
 
 v1patterns = [
      path("", include("config.urls")),
