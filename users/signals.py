a11bd24532b9495a61ca6e511a28fe91d53eda9d
Mabs Ademola <mabsademola@gmail.com>
2025-02-14 08:35:11 +0100

    #

--- users/signals.py
+++ users/signals.py
@@ -1,7 +1,7 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-
 from notification.email_service import EmailService
+from wallet.models import Wallet
 from .models import User, Profile  # Adjust according to your actual app structure
 
 email_service = EmailService()
@@ -10,6 +10,10 @@ email_service = EmailService()
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
-        email_service.send_welcome_email(instance)
+        Wallet.objects.create(user=instance)
+        try:
+            email_service.send_welcome_email(instance)
+        except:
+            print("Error sending welcome email")
         
 
