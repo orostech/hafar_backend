466dc8f8e4970c1be3be90455a014636e8527e59
Mabs Ademola <mabsademola@gmail.com>
2025-02-16 20:56:45 +0100

    #

--- subscription/admin.py
+++ subscription/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import SubscriptionPlan, UserSubscription
 
-# Register your models here.
+@admin.register(SubscriptionPlan)
+class SubscriptionPlanAdmin(admin.ModelAdmin):
+    list_display = ('name', 'coin_price', 'duration_days', 'is_active')
+    list_editable = ('coin_price', 'duration_days', 'is_active')
+
+@admin.register(UserSubscription)
+class UserSubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'plan', 'start_date', 'end_date', 'is_active')
+    search_fields = ('user__username',)
