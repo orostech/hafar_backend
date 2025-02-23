9d70f943176489dbbd67518fe79e5a11d252e7a3
Mabs Ademola <mabsademola@gmail.com>
2025-02-23 23:01:55 +0100

    #

--- hafar_backend/settings.py
+++ hafar_backend/settings.py
@@ -271,7 +271,7 @@ USE_TZ = True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-POSTMARK_API_KEY = '2a3c0088-e7fa-4e6b-95db-00c13af1ac67'
+POSTMARK_API_KEY = config('POSTMARK_API_KEY'), 
 DEFAULT_FROM_EMAIL = 'hafar.orostech@joinhafar.com'
 FRONTEND_URL = 'https://joinhafar.com'
 SITE_NAME = "Hafar"
