3b7a09a84680e04b6a2431a5b5c4fe359ec470e0
Mabs Ademola <mabsademola@gmail.com>
2025-02-23 02:08:59 +0100

    #

--- hafar_backend/settings.py
+++ hafar_backend/settings.py
@@ -1,4 +1,5 @@
 from datetime import timedelta
+import json
 import os
 from pathlib import Path
 from decouple import config, Csv
@@ -6,14 +7,10 @@ from decouple import config, Csv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# # Add after your DATABASES configuration
-# if os.name == 'nt':  # For Windows
-#     # GDAL_LIBRARY_PATH = r'C:\OSGeo4W\bin\gdal304.dll'
-#     os.environ['GDAL_DATA'] = r'C:\Program Files\GDAL\gdal-data'
-#     os.environ['GDAL_LIBRARY_PATH'] = r'C:\Program Files\GDAL\gdal303.dll'
-# else:  # For Linux/Ubuntu
-#     GDAL_LIBRARY_PATH = '/usr/lib/libgdal.so'
-#     GEOS_LIBRARY_PATH = '/usr/lib/libgeos_c.so'
+# try:
+#     FIREBASE_CREDENTIALS = json.loads(config('FIREBASE_CREDENTIALS'))
+# except json.JSONDecodeError as e:
+#     raise ValueError("Invalid JSON in FIREBASE_CREDENTIALS environment variable. Please check your .env file.") from e
 
 DEBUG = config('DEBUG', default=False, cast=bool)
 
