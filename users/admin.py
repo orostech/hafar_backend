aa67847bfecdb5db782bca00a97204996ca3da60
Mabs Ademola <mabsademola@gmail.com>
2025-02-24 13:35:40 +0100

    #

--- users/admin.py
+++ users/admin.py
@@ -11,51 +11,51 @@ class UserAdmin(admin.ModelAdmin):
     list_filter = ('is_active', 'is_staff', 'email_verified', 'phone_verified','created_at')
     readonly_fields = ('id','old_id',)
 
-
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'display_name', 'gender', 'date_of_birth', 'user_status')
     search_fields = ('user__username', 'display_name')
     list_filter = ('gender', 'user_status', 'is_verified', 'user_type', 'created_at')
-    # filter_horizontal = ('interests',)
     readonly_fields = ('updated_at', 'created_at')
+    autocomplete_fields = ('user',)
 
 @admin.register(UserPhoto)
 class UserPhotoAdmin(admin.ModelAdmin):
     list_display = ('user', 'is_primary', 'order', 'created_at')
     list_filter = ('is_primary', 'created_at')
     search_fields = ('user__username', 'caption')
+    autocomplete_fields = ('user',)
 
 @admin.register(UserVideo)
 class UserVideoAdmin(admin.ModelAdmin):
     list_display = ('user', 'title', 'video_type', 'duration', 'view_count')
     list_filter = ('video_type', 'is_public', 'created_at')
     search_fields = ('user__username', 'title', 'description')
+    autocomplete_fields = ('user',)
 
 @admin.register(UserBlock)
 class UserBlockAdmin(admin.ModelAdmin):
     list_display = ('user', 'blocked_user', 'created_at')
     search_fields = ('user__username', 'blocked_user__username')
-
-# @admin.register(Interest)
-# class InterestAdmin(admin.ModelAdmin):
-#     list_display = ('name',)
-#     search_fields = ('name',)
+    autocomplete_fields = ('user', 'blocked_user')
 
 @admin.register(UserRating)
 class UserRatingAdmin(admin.ModelAdmin):
     list_display = ('rating_user', 'rated_user', 'value', 'created_at')
     list_filter = ('value', 'created_at')
     search_fields = ('rating_user__user__username', 'rated_user__user__username')
+    autocomplete_fields = ('rating_user', 'rated_user')
 
 @admin.register(UserAudioRecording)
 class UserAudioRecordingAdmin(admin.ModelAdmin):
     list_display = ('user', 'title', 'duration', 'is_public')
     list_filter = ('is_public', 'created_at')
     search_fields = ('user__username', 'title', 'description')
+    autocomplete_fields = ('user',)
 
 @admin.register(VideoPreference)
 class VideoPreferenceAdmin(admin.ModelAdmin):
     list_display = ('profile', 'video_quality', 'autoplay_videos', 'save_to_device')
     list_filter = ('video_quality', 'autoplay_videos', 'save_to_device')
     search_fields = ('profile__user__username',)
+    autocomplete_fields = ('profile',)
